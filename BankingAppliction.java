31e31cff76c9e4f15c2c79e59da62fdd6b07d65b
honey20-04 <anishakotni2020@gmail.com>
2025-03-26 12:42:40 +0530

    Add files via upload

--- BankingAppliction.java
+++ BankingAppliction.java
@@ -1,5 +1,44 @@
 package javaproject;
+public class BankingAppliction{
+	 static int amountBalance=1000;
+	String holderName;
+	static void greetCustmor(){
+		System.out.println("Hello Welcome to Bank Website");
+		
+	}
+	void details() {
+		System.out.println("Hi Your Bank Balance :"+amountBalance);
+	}
+	static void deposite(int amount) {
+		if(amountBalance >= amount) {
+		amountBalance += amount;
+		
+		 System.out.println("your deposite amount :"+amount);
+		 System.out.println("your current Balance :"+amountBalance);
+		}
+	 }
+	 
+	void withdraw(int cash) {
+		if(amountBalance >= cash) { 
+		amountBalance -= cash;
+		
+		 System.out.println("your withdraw amount :"+cash);
+		 System.out.println("your current Balance :"+amountBalance);
+		}
+		
+	}
+	void credit() {
+		
+		
+	}
 
-public class BankingAppliction {
+	public static void main(String[] args) {
+		greetCustmor();
+		BankApplication ob = new BankApplication();
+		ob.details();
+		deposite(100);
+		ob.withdraw(200);
+		
+	}}
 
-}
+
